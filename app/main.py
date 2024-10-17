167bdf20461cf6753b22724e3beafd15e4cf034b
mymorkkis <mvsy@protonmail.com>
2024-10-17 19:19:22 +0100

    Replace deprecated app events with lifespan events
    The startup and shutdown events have now been deprecated in favor of a
    lifespan async context manager:
    https://fastapi.tiangolo.com/advanced/events/#async-context-manager

--- app/main.py
+++ app/main.py
@@ -1,20 +1,17 @@
+from contextlib import asynccontextmanager
 from fastapi import APIRouter, FastAPI
 
 from app.db import create_db_and_tables
 from app.routes import ratings, users
 
-app = FastAPI()
 
-
-@app.on_event("startup")
-async def startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     create_db_and_tables()
+    yield
 
 
-# @app.on_event("shutdown")
-# async def shutdown():
-#     await database.disconnect()
-
+app = FastAPI(lifespan=lifespan)
 
 api_router = APIRouter()
 api_router.include_router(users.router)
